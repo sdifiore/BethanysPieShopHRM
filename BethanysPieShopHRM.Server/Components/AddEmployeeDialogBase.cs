9f51301c63a31bb7a752a21dd0a504c715b5816c
Sergio Di Fiore <difiores@outlook.com>
2020-04-22 15:48:04 -0300

    AddEmployeeWidget

---
+++ BethanysPieShopHRM.Server/Components/AddEmployeeDialogBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using BethanysPieShopHRM.Server.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Server.Components
+{
+    public class AddEmployeeDialogBase : ComponentBase
+    {
+        public Employee Employee { get; set; } = new Employee { CountryId = 1, JobCategoryId = 1, BirthDate = DateTime.Now, JoinedDate = DateTime.Now };
+
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> CloseEventCallBack { get; set; }
+
+        public bool ShowDialog { get; set; }
+
+        public void Show()
+        {
+            ResetDialog();
+            ShowDialog = true;
+            StateHasChanged();
+
+        }
+
+        public void Close()
+        {
+            ShowDialog = false;
+            StateHasChanged();
+        }
+
+        public void ResetDialog()
+        {
+            Employee = new Employee { CountryId = 1, JobCategoryId = 1, BirthDate = DateTime.Now, JoinedDate = DateTime.Now };
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            await EmployeeDataService.AddEmployee(Employee);
+            await CloseEventCallBack.InvokeAsync(true);
+
+            ShowDialog = false;
+
+            StateHasChanged();
+        }
+    }
+}
--- BethanysPieShopHRM.Server/Pages/EmployeeOverviewBase.cs
+++ BethanysPieShopHRM.Server/Pages/EmployeeOverviewBase.cs
@@ -1,9 +1,10 @@
-﻿using BethanysPieShopHRM.Server.Services;
-using BethanysPieShopHRM.Shared;
-using Microsoft.AspNetCore.Components;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShopHRM.Server.Components;
+using BethanysPieShopHRM.Server.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
 
 namespace BethanysPieShopHRM.Server.Pages
 {
@@ -14,9 +15,22 @@ namespace BethanysPieShopHRM.Server.Pages
 
         public IEnumerable<Employee> Employees { get; set; }
 
+        protected AddEmployeeDialog AddEmployeeDialog { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+        }
+
+        protected void QuickAddEmployee()
+        {
+            AddEmployeeDialog.Show();
+        }
+
+        public async void AddEmployeeDialog_OnDialogClose()
+        {
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+            StateHasChanged();
         }
     }
 }
